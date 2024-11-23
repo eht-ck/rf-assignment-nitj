1ae9d7dcf35672a781e7c9d1953f4003e297fe4e
chandreshk13 <chandresh@writesonic.com>
2024-11-23 23:14:26 +0530

    chore: code clean

--- src/app.js
+++ src/app.js
@@ -1,26 +1,20 @@
 const express = require('express');
 require('dotenv').config();
 const path = require('path');
-const docRoutes = require('./api/docRoutes');  // Adjust the import path based on your directory structure
+const docRoutes = require('./api/docRoutes');  
 const app = express();
 
-// Middleware to serve static files from the 'public' directory at the root level
 app.use(express.static('public'));
-// This should now point correctly to 'public' folder
 
-// Middleware to parse JSON and URL-encoded data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Root route to serve the HTML file from the 'public' folder
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));  // Serve index.html from the public folder
+    res.sendFile(path.join(__dirname, 'public', 'index.html')); 
 });
 
-// Use docRoutes for API routes under /api/docs
 app.use('/api/docs', docRoutes);
 
-// Set up the server to listen on a specified port
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}...`);
